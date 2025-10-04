f10373e204f2fcb9712ad8d3b75fc56a6822dfb1
ValeLattanzi <vale.lattanzi15@gmail.com>
2025-10-04 16:57:25 -0300

    test(product): integration testing for service

---
+++ test/products/providers/productsProvider.ts
@@ -0,0 +1,44 @@
+import { randomInt } from 'crypto';
+import { fakeBrand } from '../fakes/brand.fake';
+import { fakeCategory } from '../fakes/category.fake';
+import { Product } from './../../../src/module/products/domain/models/product';
+export class ProductFactory {
+  /**
+   * Creates a single product for testing purposes.
+   */
+  static create(): Product {
+    const product = Product.create(
+      `Product Test`,
+      randomInt(1, 99999),
+      randomInt(0, 99999),
+      fakeBrand,
+      fakeCategory,
+    );
+
+    product.id = randomInt(1, 99999);
+    return product;
+  }
+
+  /**
+   * Creates an array of products for testing purposes.
+   * @param quantity Number of products to create
+   */
+  static createMany(quantity: number): Product[] {
+    const products: Product[] = [];
+
+    for (let i = 0; i < quantity; i++) {
+      const product = Product.create(
+        `Product ${i + 1}`,
+        randomInt(1, 99999),
+        randomInt(0, 99999),
+        fakeBrand,
+        fakeCategory,
+      );
+
+      product.id = i + 1;
+      products.push(product);
+    }
+
+    return products;
+  }
+}
